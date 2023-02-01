7769c7940786db8a7a916a609f058cd1136eabe8
kelvinndesign <122789178+kelvinndesign@users.noreply.github.com>
2023-02-01 20:47:54 +1100

    Updated files + NBA Redesign

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+const preload = document.querySelector("#preload");
+const preloadLogo = document.querySelector("#preload-logo");
 const primaryNav = document.querySelector(".primary-nav");
 const hamburger = document.querySelector("#hamburger");
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        preloadLogo.classList.add('show');
+        preloadLogo.classList.remove('hidden');
+    })
+
+    setTimeout(() => {
+        preload.classList.add('hidden')
+    }, 1000)
+})
 
 hamburger.addEventListener("click", () => {
     const visibility = primaryNav.getAttribute("data-visible");
